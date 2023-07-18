e263842e1fc12611db4eebc70d5fb9937658d561
Rediet <redietkfle@gmail.com>
2023-07-18 09:50:36 -0700

    Fix bugs

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
+"""
+Module generates square.
+"""
 
 class square():
-
+    """
+    Create empty class square.
+    """
     width = 0
     height = 0
 
-
     def __init__(self, *args, **kwargs):
+        """
+        Initialization.
+        """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,13 +22,18 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """
+        Function calculates perimeter.
+        """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """
+        Function handles string representation.
+        """
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
